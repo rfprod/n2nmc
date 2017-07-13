3d16a45d33430933df0dfc6ff4b2132c2be8e27a
rfprod <zoidenmacht@zoho.com>
2017-07-13 09:34:11 -0700

    (feature) added custom translate module consisting of: basic dictionaries, translation providers, translation service, translation pipe, translation barrel; TODO: tests

---
+++ public/app/translate/lang-en.ts
@@ -0,0 +1,10 @@
+export const LANG_EN_NAME = 'en';
+
+// keys map for English language
+export const LANG_EN_TRANSLATIONS = {
+	help: 'Help',
+	intro: 'Intro',
+	data: 'Data',
+	title: 'Application core',
+	diag: 'Platform Diagnostics'
+};
---
+++ public/app/translate/lang-ru.ts
@@ -0,0 +1,10 @@
+export const LANG_RU_NAME = 'ru';
+
+// keys map for Russian language
+export const LANG_RU_TRANSLATIONS = {
+	help: 'Помощь',
+	intro: 'Введение',
+	data: 'Данные',
+	title: 'Основа приложения',
+	diag: 'Диагностика платформы'
+};
