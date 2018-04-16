2c18ec13545c61fb0ef0db864a7cb7b0655a077e
rfprod <zoidenmacht@zoho.com>
2018-04-16 13:24:33 -0700

    (fix) client: dashboar-intro.component spec - dejunk, verify no pending requests after each it

--- test/client/components/dashboard-intro.component.spec.ts
+++ test/client/components/dashboard-intro.component.spec.ts
@@ -21,119 +21,120 @@ import { DashboardIntroComponent } from '../../../public/app/components/dashboar
 
 describe('DashboardIntroComponent', () => {
 
-		beforeEach((done) => {
-			TestBed.configureTestingModule({
-				declarations: [ TranslatePipe, NvD3Component, DashboardIntroComponent, NvD3Component ],
-				imports: [ BrowserDynamicTestingModule, NoopAnimationsModule ],
-				providers: [
-					{ provide: 'Window', useValue: { location: { host: 'localhost', protocol: 'http' } } },
-					EventEmitterService,
-					TRANSLATION_PROVIDERS,
-					TranslateService,
-					BaseRequestOptions,
-					MockBackend,
-					{ provide: Http,
-						useFactory: (mockedBackend, requestOptions) => new Http(mockedBackend, requestOptions),
-						deps: [MockBackend, BaseRequestOptions]
-					},
-					{
-						provide: PublicDataService,
-						useFactory: (http, window) => new PublicDataService(http, window),
-						deps: [Http, 'Window']
-					},
-					{
-						provide: ServerStaticDataService,
-						useFactory: (http, window) => new ServerStaticDataService(http, window),
-						deps: [Http, 'Window']
-					}
-				],
-				schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-			}).compileComponents().then(() => {
-				this.fixture = TestBed.createComponent(DashboardIntroComponent);
-				this.component = this.fixture.componentInstance;
-				spyOn(this.component, 'emitSpinnerStartEvent').and.callThrough();
-				spyOn(this.component, 'emitSpinnerStopEvent').and.callThrough();
-				this.eventEmitterSrv = TestBed.get(EventEmitterService) as EventEmitterService;
-				spyOn(this.eventEmitterSrv, 'emitEvent').and.callThrough();
-				this.serverStaticDataSrv = TestBed.get(ServerStaticDataService) as ServerStaticDataService;
-				this.publicDataSrv = TestBed.get(PublicDataService) as PublicDataService;
-				this.backend = TestBed.get(MockBackend) as MockBackend;
-				done();
-			});
-		});
-
-		it('should be defined', () => {
-			console.log(this.component);
-			expect(this.component).toBeDefined();
-		});
-
-		it('should have variables defined', () => {
-			expect(this.component.ngUnsubscribe).toEqual(jasmine.any(Subject));
-			expect(this.component.title).toBeDefined();
-			expect(this.component.title === 'Ng2NodeMongoCore (N2NMC)').toBeTruthy();
-			expect(this.component.description).toBeDefined();
-			expect(this.component.description === 'Angular, NodeJS, MongoDB').toBeTruthy();
-			expect(this.component.chartOptions).toEqual(jasmine.any(Object));
-			expect(this.component.chartOptions.chart).toBeDefined();
-			expect(this.component.chartOptions.chart).toEqual({
-				type: jasmine.any(String),
-				height: jasmine.any(Number),
-				donut: jasmine.any(Boolean),
-				x: jasmine.any(Function),
-				y: jasmine.any(Function),
-				showLabels: jasmine.any(Boolean),
-				labelSunbeamLayout: jasmine.any(Boolean),
-				pie: {
-					startAngle: jasmine.any(Function),
-					endAngle: jasmine.any(Function)
+	beforeEach((done) => {
+		TestBed.configureTestingModule({
+			declarations: [ TranslatePipe, NvD3Component, DashboardIntroComponent, NvD3Component ],
+			imports: [ BrowserDynamicTestingModule, NoopAnimationsModule ],
+			providers: [
+				{ provide: 'Window', useValue: { location: { host: 'localhost', protocol: 'http' } } },
+				EventEmitterService,
+				TRANSLATION_PROVIDERS,
+				TranslateService,
+				BaseRequestOptions,
+				MockBackend,
+				{ provide: Http,
+					useFactory: (mockedBackend, requestOptions) => new Http(mockedBackend, requestOptions),
+					deps: [MockBackend, BaseRequestOptions]
 				},
-				duration: jasmine.any(Number),
-				title: jasmine.any(String),
-				legend: {
-					margin: {
-						top: jasmine.any(Number),
-						right: jasmine.any(Number),
-						bottom: jasmine.any(Number),
-						left: jasmine.any(Number)
-					}
+				{
+					provide: PublicDataService,
+					useFactory: (http, window) => new PublicDataService(http, window),
+					deps: [Http, 'Window']
+				},
+				{
+					provide: ServerStaticDataService,
+					useFactory: (http, window) => new ServerStaticDataService(http, window),
+					deps: [Http, 'Window']
 				}
-			});
-			expect(this.component.appUsageData).toEqual(jasmine.any(Array));
-			expect(this.component.serverData).toEqual({
-				static: jasmine.any(Array),
-				dynamic: jasmine.any(Array)
-			});
-			expect(this.component.host).toBeDefined();
-			expect(this.component.host).toEqual(window.location.host);
-			expect(this.component.wsUrl).toEqual('ws://'+this.component.host+'/api/app-diag/dynamic');
-			expect(this.component.ws).toEqual(jasmine.any(WebSocket));
-			expect(this.component.getServerStaticData).toBeDefined();
-			expect(this.component.getPublicData).toBeDefined();
-			expect(this.component.emitSpinnerStartEvent).toBeDefined();
-			expect(this.component.emitSpinnerStopEvent).toBeDefined();
-			expect(this.component.nvd3).toEqual(jasmine.any(NvD3Component));
-			expect(this.component.ngOnInit).toBeDefined();
-			expect(this.component.ngOnDestroy).toBeDefined();
+			],
+			schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+		}).compileComponents().then(() => {
+			this.fixture = TestBed.createComponent(DashboardIntroComponent);
+			this.component = this.fixture.componentInstance;
+			spyOn(this.component, 'emitSpinnerStartEvent').and.callThrough();
+			spyOn(this.component, 'emitSpinnerStopEvent').and.callThrough();
+			this.eventEmitterSrv = TestBed.get(EventEmitterService) as EventEmitterService;
+			spyOn(this.eventEmitterSrv, 'emitEvent').and.callThrough();
+			this.serverStaticDataSrv = TestBed.get(ServerStaticDataService) as ServerStaticDataService;
+			this.publicDataSrv = TestBed.get(PublicDataService) as PublicDataService;
+			this.backend = TestBed.get(MockBackend) as MockBackend;
+			done();
 		});
+	});
 
-		it('emitSpinnerStartEvent should send respective event emitter message', () => {
-			this.component.emitSpinnerStartEvent();
-			expect(this.eventEmitterSrv.emitEvent).toHaveBeenCalledWith({ spinner: 'start' });
-		});
+	afterEach(() => this.backend.verifyNoPendingRequests());
 
-		it('emitSpinnerStopEvent should send respective event emitter message', () => {
-			this.component.emitSpinnerStopEvent();
-			expect(this.eventEmitterSrv.emitEvent).toHaveBeenCalledWith({ spinner: 'stop' });
-		});
+	it('should be defined', () => {
+		expect(this.component).toBeDefined();
+	});
 
-		it('should be properly destroyed', () => {
-			this.component.ngOnInit();
-			spyOn(this.component.ngUnsubscribe, 'next').and.callThrough();
-			spyOn(this.component.ngUnsubscribe, 'complete').and.callThrough();
-			this.component.ngOnDestroy();
-			expect(this.component.ngUnsubscribe.next).toHaveBeenCalled();
-			expect(this.component.ngUnsubscribe.complete).toHaveBeenCalled();
+	it('should have variables defined', () => {
+		expect(this.component.ngUnsubscribe).toEqual(jasmine.any(Subject));
+		expect(this.component.title).toBeDefined();
+		expect(this.component.title === 'Ng2NodeMongoCore (N2NMC)').toBeTruthy();
+		expect(this.component.description).toBeDefined();
+		expect(this.component.description === 'Angular, NodeJS, MongoDB').toBeTruthy();
+		expect(this.component.chartOptions).toEqual(jasmine.any(Object));
+		expect(this.component.chartOptions.chart).toBeDefined();
+		expect(this.component.chartOptions.chart).toEqual({
+			type: jasmine.any(String),
+			height: jasmine.any(Number),
+			donut: jasmine.any(Boolean),
+			x: jasmine.any(Function),
+			y: jasmine.any(Function),
+			showLabels: jasmine.any(Boolean),
+			labelSunbeamLayout: jasmine.any(Boolean),
+			pie: {
+				startAngle: jasmine.any(Function),
+				endAngle: jasmine.any(Function)
+			},
+			duration: jasmine.any(Number),
+			title: jasmine.any(String),
+			legend: {
+				margin: {
+					top: jasmine.any(Number),
+					right: jasmine.any(Number),
+					bottom: jasmine.any(Number),
+					left: jasmine.any(Number)
+				}
+			}
 		});
+		expect(this.component.appUsageData).toEqual(jasmine.any(Array));
+		expect(this.component.serverData).toEqual({
+			static: jasmine.any(Array),
+			dynamic: jasmine.any(Array)
+		});
+		expect(this.component.host).toBeDefined();
+		expect(this.component.host).toEqual(window.location.host);
+		expect(this.component.wsUrl).toEqual('ws://'+this.component.host+'/api/app-diag/dynamic');
+		expect(this.component.ws).toEqual(jasmine.any(WebSocket));
+		expect(this.component.getServerStaticData).toBeDefined();
+		expect(this.component.getPublicData).toBeDefined();
+		expect(this.component.emitSpinnerStartEvent).toBeDefined();
+		expect(this.component.emitSpinnerStopEvent).toBeDefined();
+		expect(this.component.nvd3).toEqual(jasmine.any(NvD3Component));
+		expect(this.component.ngOnInit).toBeDefined();
+		expect(this.component.ngOnDestroy).toBeDefined();
+	});
+
+	it('emitSpinnerStartEvent should send respective event emitter message', () => {
+		this.component.emitSpinnerStartEvent();
+		expect(this.eventEmitterSrv.emitEvent).toHaveBeenCalledWith({ spinner: 'start' });
+	});
+
+	it('emitSpinnerStopEvent should send respective event emitter message', () => {
+		this.component.emitSpinnerStopEvent();
+		expect(this.eventEmitterSrv.emitEvent).toHaveBeenCalledWith({ spinner: 'stop' });
+	});
+
+	it('should be properly destroyed', () => {
+		this.component.ngOnInit();
+		spyOn(this.component.ngUnsubscribe, 'next').and.callThrough();
+		spyOn(this.component.ngUnsubscribe, 'complete').and.callThrough();
+		this.component.ngOnDestroy();
+		expect(this.component.ngUnsubscribe.next).toHaveBeenCalled();
+		expect(this.component.ngUnsubscribe.complete).toHaveBeenCalled();
+	});
 
 });
 
